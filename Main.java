7005355383725940449914c30613cc5727cbd811
Arcelia Calleja <arceliacalleja@gmail.com>
2019-02-06 12:19:17 -0500

    use of scanner for Alice and Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
-/**
+import java.util.Scanner;
+/** 
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("type your name");
+        String name = scan.next();
+       
+        if (name.equals("Alice") || name.equals("Bob")){
+            System.out.println("Hello"+ name);
+        }
+        
+        
+        
+
 
     }
 }
